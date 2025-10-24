d2f920e2031403d7e02c6d4d2c191ed318b2ab71
dadler83 <113302798+dadler83@users.noreply.github.com>
2025-10-24 16:02:57 -0400

    setting up vis page

---
+++ js/viz0_story.js
@@ -0,0 +1,8 @@
+
+
+const q0_submit = document.getElementById('q0-interaction');
+const q0_submit = document.getElementById('q0-interaction');
+
+q0_submit.addEventListener('click', (e) => {
+    console.log('clicked');
+})
