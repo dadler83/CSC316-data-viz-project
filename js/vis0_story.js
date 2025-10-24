25641701eebff774e62aa696346ba82f1a5af5b6
dadler83 <113302798+dadler83@users.noreply.github.com>
2025-10-24 19:22:54 -0400

    added about section

---
+++ js/vis0_story.js
@@ -0,0 +1,15 @@
+
+
+const q0_submit = document.getElementById('q0-interaction');
+const q0 = document.getElementById('q0');
+const a0 = document.getElementById('q0-a0');
+const a1 = document.getElementById('q0-a1');
+const vis = document.getElementById('vis0');
+const gameboys = document.getElementById('gameboy-img');
+
+q0_submit.addEventListener('click', (e) => {
+    q0.style.display = 'none';
+    a0.style.display = 'block';
+    gameboys.style.display = 'none';
+    vis.style.display = 'block';
+})
--- js/viz0_story.js
+++
@@ -1,8 +0,0 @@
-
-
-const q0_submit = document.getElementById('q0-interaction');
-const q0_submit = document.getElementById('q0-interaction');
-
-q0_submit.addEventListener('click', (e) => {
-    console.log('clicked');
-})
